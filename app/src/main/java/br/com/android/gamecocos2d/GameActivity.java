0a79e669f6638308ebb9fba1b8c577d5d7b171a1
Antonio Junio <a.junio.fc@gmail.com>
2018-03-10 01:00:23 -0300

    Iniciar Projeto

---
+++ app/src/main/java/br/com/android/gamecocos2d/GameActivity.java
@@ -0,0 +1,13 @@
+package br.com.android.gamecocos2d;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
