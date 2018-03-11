096465e2795a809e507281efd84f0eb78e89318e
Antonio Junio <a.junio.fc@gmail.com>
2018-03-11 16:36:33 -0300

    Implementar prototipo

--- app/src/main/java/br/com/android/gamecocos2d/GameActivity.java
+++ app/src/main/java/br/com/android/gamecocos2d/GameActivity.java
@@ -2,12 +2,14 @@ package br.com.android.gamecocos2d;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 import br.com.android.gamecocos2d.engine.Game;
 
-public class GameActivity extends Activity {
+public class GameActivity extends Activity implements View.OnTouchListener{
     private Game mGame;
 
     @Override
@@ -20,13 +22,30 @@ public class GameActivity extends Activity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         mGame = new Game(this);
-
         setContentView(mGame);
+        mGame.setOnTouchListener(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         mGame.resume();
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+        if (event.getX() < 100 && event.getY() > 290 &&
+                event.getY() < 310) {
+            mGame.init();
+        }
+        // Exit
+        if(event.getX() < 100 && event.getY() > 490 &&
+                event.getY() < 510) {
+            System.exit(0);
+        }
+
+        mGame.moveDown(10);
+        mGame.addScore(100);
+        return true;
     }
 }
--- app/src/main/java/br/com/android/gamecocos2d/engine/Game.java
+++ app/src/main/java/br/com/android/gamecocos2d/engine/Game.java
@@ -1,32 +1,144 @@
 package br.com.android.gamecocos2d.engine;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+
+import br.com.android.gamecocos2d.R;
 
 /**
  * Created by JHUNIIN on 10/03/2018.
  */
 
 public class Game extends SurfaceView implements Runnable {
+    private Context context;
     private boolean running;
     private Thread renderThread;
+    private SurfaceHolder holder;
+    private Paint paint;
+    private int playerX = 300, playerY = 300, playerRadius = 50;
+    private int enemyX, enemyY, enemyRadius = 50;
+    private double distance;
+    private boolean gameOver;
+    private int score;
+
 
     public Game(Context context) {
         super(context);
+        this.context = context;
         this.running = false;
         this.renderThread = null;
+        this.paint = new Paint();
+        holder = getHolder();
     }
 
     @Override
     public void run() {
         while (running){
-            System.out.println("Running ....");
+            // verifica se a tela ja esta pronta
+            if (!holder.getSurface().isValid()){
+                continue;
+            }
+
+            // bloqueia o canvas
+            Canvas canvas = holder.lockCanvas();
+            canvas.drawColor(Color.BLACK);
+
+            // desenha o player
+            drawPlayer(canvas);
+            // desenha o inimigo
+            drawEnemy(canvas);
+
+            // checa se tem colisao
+            checkCollision(canvas);
+
+            if (gameOver){
+                stopGame(canvas);
+                break;
+            }
+
+            drawScore(canvas);
+
+            drawButtons(canvas);
+
+            // atualiza e libera o canvas
+            holder.unlockCanvasAndPost(canvas);
         }
+    }
+
+    private void drawPlayer(Canvas canvas) {
+        paint.setColor(Color.GREEN);
+        canvas.drawCircle(playerX, playerY, playerRadius, paint);
+    }
+
+    private void drawEnemy(Canvas canvas){
+        paint.setColor(Color.RED);
+        enemyRadius++;
+        canvas.drawCircle(enemyX, enemyY, enemyRadius, paint);
+    }
+
+    private void drawButtons(Canvas canvas){
+        // Restart
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.WHITE);
+        paint.setTextSize(30);
+        canvas.drawText("Restart", 50, 300, paint);
+        // Exit
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.WHITE);
+        paint.setTextSize(30);
+        canvas.drawText("Exit", 50, 500, paint);
+    }
+
+    public void moveDown(int pixels){
+        playerY += pixels;
+    }
+
+    private void checkCollision(Canvas canvas){
+        distance = Math.pow(playerY - enemyY, 2)
+                    + Math.pow(playerX - enemyX, 2);
+        distance =Math.sqrt(distance);
+
+        if (distance <= playerRadius + enemyRadius){
+            gameOver = true;
+        }
+    }
+
+    public void addScore(int points){
+        score += points;
+    }
+
+    public void init() {
+        enemyX = enemyY = enemyRadius = 0;
+        playerX = playerY = 300;
+        playerRadius = 50;
+        gameOver = false;
+        score = 0;
     }
 
     public void resume(){
         this.running = true;
         this.renderThread = new Thread(this);
         this.renderThread.start();
+    }
+
+    private void drawScore(Canvas canvas) {
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.WHITE);
+        paint.setTextSize(50);
+        canvas.drawText(String.valueOf(score), 50, 200, paint);
+    }
+
+    private void stopGame(Canvas canvas){
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.BLUE);
+        paint.setTextSize(100);
+        String gameOver = String.valueOf(context.getResources().getText(R.string.game_over));
+        canvas.drawText(gameOver, 150, canvas.getHeight()/2, paint);
     }
 }
